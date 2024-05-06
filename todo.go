96f56678847f0a942a7615fcf5a0757411082527
pinhob <20286747+pinhob@users.noreply.github.com>
2024-05-06 00:00:00 +0000

    feat: create Add and test

--- todo.go
+++ todo.go
@@ -1,1 +1,23 @@
 package todo
+
+import "time"
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type List []*item
+
+func (l *List) Add(task string) {
+	newTask := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, &newTask)
+}
--- todo_test.go
+++ todo_test.go
@@ -1,1 +1,19 @@
 package todo_test
+
+import (
+	"testing"
+
+	"github.com/pinhob/todo-go"
+)
+
+func TestAdd(t *testing.T) {
+	ls := todo.List{}
+	task := "task 1"
+
+	ls.Add(task)
+	got := ls[0].Task
+
+	if got != task {
+		t.Errorf("got %s want %s", got, task)
+	}
+}
