deab0c13fa7eda09f3e7316875e5d06c3deb4cb1
pinhob <20286747+pinhob@users.noreply.github.com>
2024-05-15 19:04:34 -0300

    feat: create post route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/pinhob/todo-go"
 )
 
+const todoFileName = "../todo/.todo.json"
+
+type task struct {
+	Description string `json:"task"`
+}
+
 func main() {
 	http.HandleFunc("/", handleRoot)
 	http.HandleFunc("/todos", handleTodos)
@@ -22,18 +28,44 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 func handleTodos(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	default:
+	case http.MethodGet:
 		listTodos(w, r)
+	case http.MethodPost:
+		addTodo(w, r)
 	}
 }
 
 func listTodos(w http.ResponseWriter, r *http.Request) {
 	ls := &todo.List{}
-	if err := ls.Load("../todo/.todo.json"); err != nil {
+	if err := ls.Load(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(ls)
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	var task task
+	ls := &todo.List{}
+	if err := ls.Load(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err)
+	}
+
+	ls.Add(task.Description)
+	if err := ls.Save(todoFileName); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
 }
